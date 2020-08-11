d3c35e2b556e593c180e257cf7ca9deb0bac7b71
RobertEverett <36086144+RobertEverett@users.noreply.github.com>
2020-08-11 09:08:15 -0500

    Update Account.js

--- FormValidation/Account.js
+++ FormValidation/Account.js
@@ -6,39 +6,39 @@ var confirminput = document.getElementById('Confirm')
 function validate()  {
     
     if (nameinput.value === '' || nameinput.value == null){
-        document.getElementById("error1").style.backgroundColor="#ff4747";
-        document.getElementById("error1").style.border="1px solid white";
-        document.getElementById("errortext1").textContent = 'Name is required';
+        document.querySelector("#error1").style.backgroundColor="#ff4747";
+        document.querySelector("#error1").style.border="1px solid white";
+        document.querySelector("#errortext1").textContent = 'Name is required';
     
     }else if (nameinput.value !== ''){
-        document.getElementById("error1").style.backgroundColor='';
-        document.getElementById("error1").style.border="";
-        document.getElementById("errortext1").textContent = '';
+        document.querySelector("#error1").style.backgroundColor='';
+        document.querySelector("#error1").style.border="";
+        document.querySelector("#errortext1").textContent = '';
     
     }  if (passwordinput.value === '') {
-        document.getElementById("error2").style.backgroundColor="#ff4747";
-        document.getElementById("error2").style.border="1px solid white";
-        document.getElementById("errortext2").textContent = 'Please enter a password';
+        document.querySelector("#error2").style.backgroundColor="#ff4747";
+        document.querySelector("#error2").style.border="1px solid white";
+        document.querySelector("#errortext2").textContent = 'Please enter a password';
 
     } else if (passwordinput.value.length >= 1 && passwordinput.value.length < 8 || passwordinput.value.length > 20) {
-        document.getElementById("error2").style.backgroundColor="#ff4747";
-        document.getElementById("error2").style.border="1px solid white";
-        document.getElementById("errortext2").textContent ='Password must be at least 8 characters in length but no more than 20 characters in length';
+        document.querySelector("#error2").style.backgroundColor="#ff4747";
+        document.querySelector("#error2").style.border="1px solid white";
+        document.querySelector("#errortext2").textContent ='Password must be at least 8 characters in length but no more than 20 characters in length';
         event.preventDefault();
     } else if (passwordinput.value.length >= 8 && passwordinput.value.length <= 20) {
-        document.getElementById("error2").style.backgroundColor="";
-        document.getElementById("error2").style.border="";
-        document.getElementById("errortext2").textContent ='';
+        document.querySelector("#error2").style.backgroundColor="";
+        document.querySelector("#error2").style.border="";
+        document.querySelector("#errortext2").textContent ='';
         
     } if(confirminput.value === '' || confirminput.value !== passwordinput.value) {
-        document.getElementById("error3").style.backgroundColor="#ff4747";
-        document.getElementById("error3").style.border="1px solid white";
-        document.getElementById("errortext3").textContent = 'Passwords must match'
+        document.querySelector("#error3").style.backgroundColor="#ff4747";
+        document.querySelector("#error3").style.border="1px solid white";
+        document.querySelector("#errortext3").textContent = 'Passwords must match'
        event.preventDefault();
     }else if(confirminput.value === passwordinput.value) {
-        document.getElementById("error3").style.backgroundColor="";
-        document.getElementById("error3").style.border="";
-        document.getElementById("errortext3").textContent = "";
+        document.querySelector("#error3").style.backgroundColor="";
+        document.querySelector("#error3").style.border="";
+        document.querySelector("#errortext3").textContent = "";
 
     }else if(confirminput.value !== passwordinput.value) {
     event.preventDefault();
@@ -46,7 +46,7 @@ function validate()  {
 };
 
 function init() {
-    document.getElementById('form').onsubmit = validate();
+    document.querySelector('#form').onsubmit = validate();
 };
 
 
