bc4a7d63d0f6bff049702a39fc2fa0599ee18286
RobertEverett <36086144+RobertEverett@users.noreply.github.com>
2020-08-16 14:33:36 -0500

    Add files via upload

---
+++ ToDoList/app.js
@@ -0,0 +1,34 @@
+var date = new Date();
+var TimeStamp = date.toLocaleString();
+
+function AddToList(){
+var item = document.querySelector("#inputfield").value
+var text = document.createTextNode(item)
+var newItem = document.createElement("li")
+
+newItem.appendChild(text)
+document.getElementById("Tasks").appendChild(newItem)
+}
+
+
+function checklist(){
+    var listlength=document.getElementById("Tasks").querySelectorAll("li").length;
+    if(listlength === 10){
+    document.querySelector("#addbtn").style.pointerEvents="none";
+    document.querySelector("#addbtn").style.opacity=".4";
+ } 
+}
+
+function ClearList(){
+document.getElementById("Tasks").innerHTML = "";
+document.querySelector("#addbtn").style.pointerEvents="auto";
+if(document.querySelector("#addbtn").style.opacity !== 1){
+    document.querySelector("#addbtn").style.opacity="1"  
+}
+}
+
+function CurrentTime(){
+document.getElementById('datecontainer').innerHTML=TimeStamp;
+}
+
+CurrentTime();
